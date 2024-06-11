4b2194ef0976572c676e3ac6b0d7eb7cb6369aae
Sarwak <sarwakpro@gmail.com>
2024-06-11 19:50:20 -0400

    se agregaron nuevas funciones

--- src/main.c
+++ src/main.c
@@ -9,11 +9,12 @@
 // librerias creadas
 #include "items/items.h"
 
-void mostrarMenu() {
-    printf("\nMenu:\n");
-    printf("1. Dar de comer pan al Tamagotchi\n");
-    printf("2. Salir del programa\n");
-}
+
+void menuPrincipal(struct Tamagotchi mascota);
+void menuInferior();
+void menuInventario();
+void limpiarPantalla();
+
 
 int main() {
     // Crear un Tamagotchi
@@ -29,41 +30,84 @@ int main() {
     struct Item agua;
     strcpy(agua.nombre, "Agua");
 
+    
+
     int opcion;
 
-     do {
-        // Mostrar el estado actual del Tamagotchi
-        printf("\nEstado actual del Tamagotchi:\n");
-        printf(" /\\_/\\   \n");
-        printf("( o o )  \n");
-        printf(" > ^ <   \n\n");
-        printf("Hambre: %d\n", mascota.hambre);
-        printf("Cansancio: %d\n", mascota.cansancio);
-        printf("Aburrimiento: %d\n", mascota.aburrimiento);
-
-        // Mostrar el menú
-        mostrarMenu();
-        
-        // Leer la opción seleccionada por el usuario
-        printf("\nSelecciona una opcion: ");
-        scanf("%d", &opcion);
-
-        // Realizar la acción correspondiente según la opción seleccionada
-        switch(opcion) {
+    menuPrincipal(mascota);
+    menuInferior();
+    
+    printf("\nSelecciona una opcion: \n");
+    scanf("%d", &opcion);
+    switch(opcion) {
             case 1:
-                // Aplicar efecto del pan a la mascota
-                aplicarEfecto(&manzana, &mascota);
-                printf("\nHas dado de comer una manzana al Tamagotchi. ¡Qué delicia!\n");
+                //Inventario
+                limpiarPantalla();
+                menuInventario();
+                printf("\nSelecciona una opcion: \n");
+                scanf("%d", &opcion);
+                switch(opcion) {
+                    case 1:
+                        //Manzana
+                        printf("\nHas comido una manzana te quedan 5");
+                        scanf("%d", &opcion);
+                        break;
+                    case 2:
+                        //Agua
+                        printf("\nHas tomado un Agua te quedan 5");
+                        getchar();
+                        break;
+                    case 3:
+                        //Coca-Cola
+                        printf("\nHas tomado una Coca-Cola te quedan 5");
+                        getchar();
+                    case 4:
+                        //Salir
+                        break;
+                    default:
+                        // Opción no válida
+                        printf("\nOpcion no valida. Por favor, selecciona una opcion valida.\n");
+                    }
                 break;
             case 2:
-                // Salir del programa
-                printf("\nSaliendo del programa...\n");
+                //Jugar
+                limpiarPantalla();
+                printf("Proximamente...");
                 break;
+            case 3:
+                //Salir
+                printf("\nSaliendo del programa...\n");
             default:
                 // Opción no válida
                 printf("\nOpcion no valida. Por favor, selecciona una opcion valida.\n");
         }
-    } while(opcion != 2); // Continuar mostrando el menú hasta que el usuario seleccione la opción de salir
 
     return 0;
 }
+
+void menuPrincipal(struct Tamagotchi mascota) {
+    printf("\nEstado actual del Tamagotchi:\n");
+    printf(" /\\_/\\   \n");
+    printf("( o o )  \n");
+    printf(" > ^ <   \n\n");
+    printf("Hambre: %d\n", mascota.hambre);
+    printf("Cansancio: %d\n", mascota.cansancio);
+    printf("Aburrimiento: %d\n", mascota.aburrimiento);
+}
+
+void menuInferior() {
+    printf("\n\nSelecciona una opcion:\n\n");
+    printf("1) Abrir Inventario\n");
+    printf("2) Jugar\n");
+    printf("3) Dormir\n");
+}
+
+void menuInventario() {
+    printf("\n\nSelecciona una opcion:\n\n");
+    printf("1) Manzana x5\n");
+    printf("2) Agua x2\n");
+    printf("3) Coca-Cola x6\n");
+    printf("4) Salir\n");
+}
+
+void limpiarPantalla() { system("cls"); }
