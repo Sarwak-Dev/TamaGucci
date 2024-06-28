140dbd5c3220eb39352e2fe1f7432d640700a2e1
InstitucionalFelipe <163430542+InstitucionalFelipe@users.noreply.github.com>
2024-06-27 21:03:50 -0400

    animacion

--- src/img/design.c
+++ src/img/design.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
-#include <time.h>
+#include <unistd.h>
 
 #include "design.h"
 #include "../functions/structs/structs.h"
